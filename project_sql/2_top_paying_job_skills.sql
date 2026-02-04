7ffe45a2b3bb46000f80d6e65c5d947d39c09abe
OtherDann <dann.jeffries@hotmail.co.uk>
2026-02-04 10:21:25 +0000

    add readme and updated query

--- project_sql/2_top_paying_job_skills.sql
+++ project_sql/2_top_paying_job_skills.sql
@@ -1,45 +1,351 @@
-"job_id","job_title","salary_year_avg","company_name","skills"
-21321,"Data Engineer","325000.0","Engtal","python"
-21321,"Data Engineer","325000.0","Engtal","spark"
-21321,"Data Engineer","325000.0","Engtal","pandas"
-21321,"Data Engineer","325000.0","Engtal","numpy"
-21321,"Data Engineer","325000.0","Engtal","pyspark"
-21321,"Data Engineer","325000.0","Engtal","hadoop"
-21321,"Data Engineer","325000.0","Engtal","kafka"
-21321,"Data Engineer","325000.0","Engtal","kubernetes"
-157003,"Data Engineer","325000.0","Engtal","python"
-157003,"Data Engineer","325000.0","Engtal","spark"
-157003,"Data Engineer","325000.0","Engtal","pandas"
-157003,"Data Engineer","325000.0","Engtal","numpy"
-157003,"Data Engineer","325000.0","Engtal","pyspark"
-157003,"Data Engineer","325000.0","Engtal","hadoop"
-157003,"Data Engineer","325000.0","Engtal","kafka"
-157003,"Data Engineer","325000.0","Engtal","kubernetes"
-270455,"Data Engineer","300000.0","Durlston Partners","sql"
-270455,"Data Engineer","300000.0","Durlston Partners","python"
-230458,"Director of Engineering - Data Platform","251000.0","Twitch","spark"
-230458,"Director of Engineering - Data Platform","251000.0","Twitch","hadoop"
-230458,"Director of Engineering - Data Platform","251000.0","Twitch","kafka"
-230458,"Director of Engineering - Data Platform","251000.0","Twitch","tensorflow"
-230458,"Director of Engineering - Data Platform","251000.0","Twitch","keras"
-230458,"Director of Engineering - Data Platform","251000.0","Twitch","pytorch"
-595768,"Principal Data Engineer","250000.0","Signify Technology","python"
-595768,"Principal Data Engineer","250000.0","Signify Technology","scala"
-595768,"Principal Data Engineer","250000.0","Signify Technology","databricks"
-595768,"Principal Data Engineer","250000.0","Signify Technology","spark"
-561728,"Data Engineer","250000.0","AI Startup","python"
-561728,"Data Engineer","250000.0","AI Startup","scala"
-561728,"Data Engineer","250000.0","AI Startup","r"
-561728,"Data Engineer","250000.0","AI Startup","azure"
-543728,"Staff Data Engineer","250000.0","Signify Technology","python"
-543728,"Staff Data Engineer","250000.0","Signify Technology","scala"
-543728,"Staff Data Engineer","250000.0","Signify Technology","databricks"
-543728,"Staff Data Engineer","250000.0","Signify Technology","spark"
-151972,"Principal Data Engineer (Remote)","245000.0","Movable Ink","nosql"
-151972,"Principal Data Engineer (Remote)","245000.0","Movable Ink","aws"
-151972,"Principal Data Engineer (Remote)","245000.0","Movable Ink","gcp"
-204320,"Staff Data Engineer","245000.0","Handshake","go"
-2446,"Data Engineering Manager","242000.0","Meta","sql"
-2446,"Data Engineering Manager","242000.0","Meta","python"
-2446,"Data Engineering Manager","242000.0","Meta","java"
-2446,"Data Engineering Manager","242000.0","Meta","perl"
+/*
+QUESTION: What skills are required for the top-paying Data Engineer jobs?
+- Use the Top 10 highest-paying Data Engineer jobs from the first query.
+- Add specific skills required for these roles.
+- Why? It provides a detailed look at which high-paying jobs demand certain skills,
+    helping job-seekers understand which skills to develop that align with top salaries.
+*/
+
+WITH top_paying_jobs AS (
+SELECT
+    job_id,
+    job_title,
+    salary_year_avg,
+    name AS company_name
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Engineer' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT
+    10
+)
+
+SELECT
+    top_paying_jobs.*,
+    skills_dim.skills
+FROM
+    top_paying_jobs
+INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY
+    salary_year_avg DESC;
+
+/* RESULTS AS A JSON
+
+[
+  {
+    "job_id": 21321,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "325000.0",
+    "company_name": "Engtal",
+    "skills": "python"
+  },
+  {
+    "job_id": 21321,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "325000.0",
+    "company_name": "Engtal",
+    "skills": "spark"
+  },
+  {
+    "job_id": 21321,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "325000.0",
+    "company_name": "Engtal",
+    "skills": "pandas"
+  },
+  {
+    "job_id": 21321,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "325000.0",
+    "company_name": "Engtal",
+    "skills": "numpy"
+  },
+  {
+    "job_id": 21321,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "325000.0",
+    "company_name": "Engtal",
+    "skills": "pyspark"
+  },
+  {
+    "job_id": 21321,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "325000.0",
+    "company_name": "Engtal",
+    "skills": "hadoop"
+  },
+  {
+    "job_id": 21321,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "325000.0",
+    "company_name": "Engtal",
+    "skills": "kafka"
+  },
+  {
+    "job_id": 21321,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "325000.0",
+    "company_name": "Engtal",
+    "skills": "kubernetes"
+  },
+  {
+    "job_id": 157003,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "325000.0",
+    "company_name": "Engtal",
+    "skills": "python"
+  },
+  {
+    "job_id": 157003,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "325000.0",
+    "company_name": "Engtal",
+    "skills": "spark"
+  },
+  {
+    "job_id": 157003,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "325000.0",
+    "company_name": "Engtal",
+    "skills": "pandas"
+  },
+  {
+    "job_id": 157003,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "325000.0",
+    "company_name": "Engtal",
+    "skills": "numpy"
+  },
+  {
+    "job_id": 157003,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "325000.0",
+    "company_name": "Engtal",
+    "skills": "pyspark"
+  },
+  {
+    "job_id": 157003,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "325000.0",
+    "company_name": "Engtal",
+    "skills": "hadoop"
+  },
+  {
+    "job_id": 157003,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "325000.0",
+    "company_name": "Engtal",
+    "skills": "kafka"
+  },
+  {
+    "job_id": 157003,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "325000.0",
+    "company_name": "Engtal",
+    "skills": "kubernetes"
+  },
+  {
+    "job_id": 270455,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "300000.0",
+    "company_name": "Durlston Partners",
+    "skills": "sql"
+  },
+  {
+    "job_id": 270455,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "300000.0",
+    "company_name": "Durlston Partners",
+    "skills": "python"
+  },
+  {
+    "job_id": 230458,
+    "job_title": "Director of Engineering - Data Platform",
+    "salary_year_avg": "251000.0",
+    "company_name": "Twitch",
+    "skills": "spark"
+  },
+  {
+    "job_id": 230458,
+    "job_title": "Director of Engineering - Data Platform",
+    "salary_year_avg": "251000.0",
+    "company_name": "Twitch",
+    "skills": "hadoop"
+  },
+  {
+    "job_id": 230458,
+    "job_title": "Director of Engineering - Data Platform",
+    "salary_year_avg": "251000.0",
+    "company_name": "Twitch",
+    "skills": "kafka"
+  },
+  {
+    "job_id": 230458,
+    "job_title": "Director of Engineering - Data Platform",
+    "salary_year_avg": "251000.0",
+    "company_name": "Twitch",
+    "skills": "tensorflow"
+  },
+  {
+    "job_id": 230458,
+    "job_title": "Director of Engineering - Data Platform",
+    "salary_year_avg": "251000.0",
+    "company_name": "Twitch",
+    "skills": "keras"
+  },
+  {
+    "job_id": 230458,
+    "job_title": "Director of Engineering - Data Platform",
+    "salary_year_avg": "251000.0",
+    "company_name": "Twitch",
+    "skills": "pytorch"
+  },
+  {
+    "job_id": 543728,
+    "job_title": "Staff Data Engineer",
+    "salary_year_avg": "250000.0",
+    "company_name": "Signify Technology",
+    "skills": "python"
+  },
+  {
+    "job_id": 543728,
+    "job_title": "Staff Data Engineer",
+    "salary_year_avg": "250000.0",
+    "company_name": "Signify Technology",
+    "skills": "scala"
+  },
+  {
+    "job_id": 543728,
+    "job_title": "Staff Data Engineer",
+    "salary_year_avg": "250000.0",
+    "company_name": "Signify Technology",
+    "skills": "databricks"
+  },
+  {
+    "job_id": 543728,
+    "job_title": "Staff Data Engineer",
+    "salary_year_avg": "250000.0",
+    "company_name": "Signify Technology",
+    "skills": "spark"
+  },
+  {
+    "job_id": 561728,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "250000.0",
+    "company_name": "AI Startup",
+    "skills": "python"
+  },
+  {
+    "job_id": 561728,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "250000.0",
+    "company_name": "AI Startup",
+    "skills": "scala"
+  },
+  {
+    "job_id": 561728,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "250000.0",
+    "company_name": "AI Startup",
+    "skills": "r"
+  },
+  {
+    "job_id": 561728,
+    "job_title": "Data Engineer",
+    "salary_year_avg": "250000.0",
+    "company_name": "AI Startup",
+    "skills": "azure"
+  },
+  {
+    "job_id": 595768,
+    "job_title": "Principal Data Engineer",
+    "salary_year_avg": "250000.0",
+    "company_name": "Signify Technology",
+    "skills": "python"
+  },
+  {
+    "job_id": 595768,
+    "job_title": "Principal Data Engineer",
+    "salary_year_avg": "250000.0",
+    "company_name": "Signify Technology",
+    "skills": "scala"
+  },
+  {
+    "job_id": 595768,
+    "job_title": "Principal Data Engineer",
+    "salary_year_avg": "250000.0",
+    "company_name": "Signify Technology",
+    "skills": "databricks"
+  },
+  {
+    "job_id": 595768,
+    "job_title": "Principal Data Engineer",
+    "salary_year_avg": "250000.0",
+    "company_name": "Signify Technology",
+    "skills": "spark"
+  },
+  {
+    "job_id": 204320,
+    "job_title": "Staff Data Engineer",
+    "salary_year_avg": "245000.0",
+    "company_name": "Handshake",
+    "skills": "go"
+  },
+  {
+    "job_id": 151972,
+    "job_title": "Principal Data Engineer (Remote)",
+    "salary_year_avg": "245000.0",
+    "company_name": "Movable Ink",
+    "skills": "nosql"
+  },
+  {
+    "job_id": 151972,
+    "job_title": "Principal Data Engineer (Remote)",
+    "salary_year_avg": "245000.0",
+    "company_name": "Movable Ink",
+    "skills": "aws"
+  },
+  {
+    "job_id": 151972,
+    "job_title": "Principal Data Engineer (Remote)",
+    "salary_year_avg": "245000.0",
+    "company_name": "Movable Ink",
+    "skills": "gcp"
+  },
+  {
+    "job_id": 609418,
+    "job_title": "Data Engineering Manager",
+    "salary_year_avg": "242000.0",
+    "company_name": "Meta",
+    "skills": "sql"
+  },
+  {
+    "job_id": 609418,
+    "job_title": "Data Engineering Manager",
+    "salary_year_avg": "242000.0",
+    "company_name": "Meta",
+    "skills": "python"
+  },
+  {
+    "job_id": 609418,
+    "job_title": "Data Engineering Manager",
+    "salary_year_avg": "242000.0",
+    "company_name": "Meta",
+    "skills": "java"
+  },
+  {
+    "job_id": 609418,
+    "job_title": "Data Engineering Manager",
+    "salary_year_avg": "242000.0",
+    "company_name": "Meta",
+    "skills": "perl"
+  }
+]
+
+*/
